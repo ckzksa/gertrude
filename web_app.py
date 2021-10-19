af37ed1198bbe55a5346146bc9d35af8f302fb79
ckzksa <kevin.cerkiewicz@student.uliege.be>
2021-10-19 23:24:14 +0200

    web app added

---
+++ web_app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Gertrude"
+
+def run():
+  app.run(host='0.0.0.0', port=8080)
+
+def start_thread():
+    t = Thread(target=run)
+    t.start()
+
+if __name__ == "__main__":
+  run()
+  
